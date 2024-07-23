716c552aaee369584ff39341af82927a835f6189
Renukta-21 <edu211004@gmail.com>
2024-07-23 16:42:41 -0600

    Fields mapping

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,23 +10,47 @@ function App() {
     phoneNumber: null,
     position: null,
     salary: null,
-    actions: null
+    actions: null,
+    newField:null
   }
 
   const [newEmployee, setNewEmployee] = useState(employeeMockup)
   const handleNewEmployee = () => {
     console.log('ajam')
   }
+  const handleChange =(e)=>{
+    const fieldToSet = e.target.id.split('-')[1]
+    setNewEmployee(prevData=>{
+      console.log(prevData)
+      return({
+        ...prevData,
+        [fieldToSet ]: e.target.value
+      })
+    })
+    
+  }
   const entries = Object.keys(employeeMockup)
-  console.log(entries)
+  
+  const readableFieldNames = {
+    id: "ID",
+    fullName: "Full Name",
+    gender: "Gender",
+    email: "Email",
+    phoneNumber: "Phone Number",
+    position: "Position",
+    salary: "Salary",
+    actions: "Actions",
+    newField: "New Field"
+  };
+
   return (
     <div className='bg-gray-700 min-h-screen text-white flex items-center flex-col'>
       <h1 className='text-3xl font-bold'>CRUD Employee List</h1>
       <p>Simple CRUD MongoBG application</p>
-      <Table>
+      <Table entries={entries} readableFieldNames={readableFieldNames}>
         <tr>
-          {entries.map(field => (
-            <td><input type="text" /></td>
+          {entries.map((field, idx) => (
+            <td key={idx}><input type="text" id={`input-${field}`} onChange={handleChange}/></td>
           ))}
           <td className="whitespace-nowrap flex justify-center px-20">
             <button className="bg-green-600 p-2"
@@ -38,20 +62,15 @@ function App() {
   )
 }
 
-function Table({ children }) {
+function Table({ children, entries, readableFieldNames }) {
   return (
     <div>
       <table className="employeeTable">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Full Name</th>
-            <th>Gender</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Position</th>
-            <th>Salary</th>
-            <th>Actions</th>
+            {entries.map((field,idx) => (
+              <td key={idx}>{readableFieldNames[field]}</td>
+            ))}
           </tr>
         </thead>
         <tbody>
