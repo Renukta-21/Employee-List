199d4a6f83234da85acb00b35101efb0744946bf
Renukta-21 <edu211004@gmail.com>
2024-07-22 23:20:45 -0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+
+function App() {
+  const employeeMockup = {
+    id: null,
+    fullName: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+    position: null,
+    salary: null,
+    actions: null
+  }
+
+  const [newEmployee, setNewEmployee] = useState(employeeMockup)
+  const handleNewEmployee=()=>{
+    
+  }
+  return (
+    <div className='bg-gray-700 min-h-screen text-white flex items-center flex-col'>
+      <h1 className='text-3xl font-bold'>CRUD Employee List</h1>
+      <p>Simple CRUD MongoBG application</p>
+      <Table>
+        <tr>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td className="whitespace-nowrap">
+            <button className="bg-green-600 p-2"
+            onClick={handleNewEmployee}>Add new employee</button>
+          </td>
+        </tr>
+      </Table >
+    </div>
+  )
+}
+
+function Table({ children }) {
+  return (
+    <div>
+      <table className="employeeTable">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Position</th>
+            <th>Salary</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {children}
+        </tbody>
+      </table>
+    </div>)
+}
+
+export default App
